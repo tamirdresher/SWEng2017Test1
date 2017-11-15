2c05734da2f129a2b8f9de3a8bef4b5e73ebf6b7
Tamir Dresher <tamir.dresher@gmail.com>
2017-11-15 09:45:11 +0200

    making a failed commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello   1");
+            Console.WriteLine("Hello   1")
         }
     }
 }
